7658964e3e1c6095c91f642e194a864b5bbff0a4
phanik <gacigogi3000@naver.com>
2024-03-22 14:23:54 +0900

    add remove

--- Shop/src/shop/Shop.java
+++ Shop/src/shop/Shop.java
@@ -8,30 +8,32 @@ public class Shop {
 	private Scanner scan = new Scanner(System.in);
 	private UserManager userManager;
 	private ItemManager itemManger;
-	private final int USER= 1;
-	private final int FILE= 2;
-	private final int ADMIN= 3;
-	
+	private final int USER = 1;
+	private final int FILE = 2;
+	private final int ADMIN = 3;
+
 	private final int JOIN = 1;
 	private final int OUT = 2;
 	private final int LOG_IN = 3;
 	private final int LOG_OUT = 4;
 	private final int SHOPPING = 5;
-	
+
 	private final int ITEM = 1;
 	private final int CHECK = 2;
-	
+
 	private int log;
-	
+
 	public Shop(String brand) {
 		this.brand = brand;
 	}
+
 	private void setSystem() {
 		log = -1;
 	}
+
 	private int inputNumber(String message) {
 		System.out.println(message);
-		int number = 0 ;
+		int number = 0;
 		try {
 			String input = scan.next();
 			number = Integer.parseInt(input);
@@ -40,54 +42,27 @@ public class Shop {
 		}
 		return number;
 	}
+
 	private String inputString(String message) {
 		System.out.println(message);
 		return scan.next();
 	}
+
 	private void printMainMenu() {
 		System.out.println("[1]유저메뉴");
 		System.out.println("[2]파일메뉴");
 		System.out.println("[3]관리자메뉴");
 	}
-	
+
 	private void runMainMenu() {
 		int sel = inputNumber("메뉴를 선택해주세요");
-		if(sel == USER)
+		if (sel == USER)
 			runUserMenu();
-		else if(sel == FILE)
-			else if(sel == ADMIN)
-				runAdminMenu();
-		
+		else if (sel == ADMIN)
+			runAdminMenu();
+
 	}
-	private void runUserMenu() {
-		int sel = inputNumber("메뉴를 선택해주세요");
-		if(sel == JOIN)
-			userManager.addUser();
-		else if(sel == OUT)
-			userManager.removeUser(log);
-			else if(sel == LOG_IN)
-				userManager
-				else if(sel == LOG_OUT)
-					else if(sel == SHOPPING)
-		
-	}
-	private void logIn() {
-		String id = inputString("ID를 입력해주세요");
-		String pw = inputString("비밀번호를 입력해주세요");
-		int index = userManager.checkUserInfo(id, pw);
-		if(index ==-1) {
-			System.err.println("회원정보가 일치하지 않습니다");
-			return;
-		}
-		
-		log = index;
-	}
-	private void runAdminMenu(){
-		int sel = inputNumber("메뉴를 선택해주세요");
-		if(sel == ITEM)
-			userManager.addUser();
-		else if(sel == CHECK)
-	}
+
 	private void printUserMenu() {
 		System.out.println("[1]회원가입");
 		System.out.println("[2]회원탈퇴");
@@ -95,43 +70,89 @@ public class Shop {
 		System.out.println("[4]로그아웃");
 		System.out.println("[5]쇼핑하기");
 	}
+
+	private void runUserMenu() {
+		int sel = inputNumber("메뉴를 선택해주세요");
+		if (sel == JOIN)
+			userManager.addUser();
+		else if (sel == OUT)
+			userManager.removeUser(log);
+		else if (sel == LOG_IN)
+			logIn();
+		else if (sel == LOG_OUT)
+			logOut();
+		else if (sel == SHOPPING)
+			shopping();
+
+	}
+
+	private void logIn() {
+		String id = inputString("ID를 입력해주세요");
+		String pw = inputString("비밀번호를 입력해주세요");
+		int index = userManager.checkUserInfo(id, pw);
+		if (index == -1) {
+			System.err.println("회원정보가 일치하지 않습니다");
+			return;
+		}
+
+		log = index;
+	}
+
+	private void logOut() {
+		log = -1;
+		System.out.println("로그아웃 되었습니다");
+	}
+
+	private void shopping() {
+
+	}
+
+	private void runAdminMenu(){
+		int sel = inputNumber("메뉴를 선택해주세요");
+		if(sel == ITEM)
+			userManager.addUser();
+		else if(sel == CHECK)
+	}
+
 	private void printMyPage() {
 		System.out.println("[1]장바구니");
 		System.out.println("[2]항목삭제");
 		System.out.println("[3]수량수정");
 		System.out.println("[4]결제");
 	}
+
 	private void printAdminMenu() {
 		System.out.println("[1]아이템");
 		System.out.println("[2]조회");
 	}
-	
+
 	private void printItemMenu() {
 		System.out.println("[1]등록");
 		System.out.println("[1]삭제");
 		System.out.println("[1]수정");
 	}
+
 	public void run() {
-		// 유저 - 
-		// ㄴ 회원가입					[O]
-		// ㄴ 탈퇴					[X]
-		// ㄴ 로그인					[X]
-		// ㄴ 로그아웃					[X]
-		// ㄴ 쇼핑하기					[X]
+		// 유저 -
+		// ㄴ 회원가입 [O]
+		// ㄴ 탈퇴 [X]
+		// ㄴ 로그인 [X]
+		// ㄴ 로그아웃 [X]
+		// ㄴ 쇼핑하기 [X]
 		// ㄴ 마이페이지
-		//   ㄴ 내장바구니				[X]
-		//   ㄴ 항목삭제				[X]
-		//   ㄴ 수량수정				[X]
-		//   ㄴ 결제					[X]
-		// 파일 
-		// ㄴ 자동저장					[X]
-		// ㄴ 자동로드					[X]
-		
+		// ㄴ 내장바구니 [X]
+		// ㄴ 항목삭제 [X]
+		// ㄴ 수량수정 [X]
+		// ㄴ 결제 [X]
+		// 파일
+		// ㄴ 자동저장 [X]
+		// ㄴ 자동로드 [X]
+
 		// 관리자 -
-		// ㄴ 아이템		
-		//   ㄴ 등록					[X]
-		//   ㄴ 삭제					[X]
-		//   ㄴ 수정					[X]
-		// ㄴ 조회(총 매출)			[X]
+		// ㄴ 아이템
+		// ㄴ 등록 [X]
+		// ㄴ 삭제 [X]
+		// ㄴ 수정 [X]
+		// ㄴ 조회(총 매출) [X]
 	}
 }
--- Shop/src/shop/UserManager.java
+++ Shop/src/shop/UserManager.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 
 public class UserManager {
 	private Scanner scan = new Scanner(System.in);
-	private ArrayList<User>users;
+	private ArrayList<User> users;
+
 	private int inputNumber(String message) {
 		System.out.println(message);
-		int number = 0 ;
+		int number = 0;
 		try {
 			String input = scan.next();
 			number = Integer.parseInt(input);
@@ -17,50 +18,55 @@ public class UserManager {
 		}
 		return number;
 	}
-	
+
 	private String inputString(String message) {
 		System.out.println(message);
 		return scan.next();
 	}
+
 	public void addUser() {
-		
+
 		String id = inputString("아이디를 입력해주세요");
-		if(!checkIdDuplication(id)) {
+		if (!checkIdDuplication(id)) {
 			System.err.println("중복된 아이디입니다");
 			return;
 		}
 		String pw = inputString("비밀번호를 입력해주세요");
-		
-		User user = new User(id,pw);
+
+		User user = new User(id, pw);
 		users.add(user);
 	}
+
 	public void removeUser(int index) {
 		String pw = inputString("비밀번호를 입력해주세요");
 		User user = users.get(index);
-		if(!user.getPw().equals(pw)) {
+		if (!user.getPw().equals(pw)) {
 			System.err.println("비밀번호가 일치하지 않습니다");
 			return;
 		}
 		users.remove(index);
 	}
+
 	public void logIn() {
 		String id = inputString("ID를 입력해주세요");
-		
+
 	}
+
 	private boolean checkIdDuplication(String id) {
-		
-		for(int i = 0 ; i<users.size();i++) {
-		User user = users.get(i);
-		if(user.getId().equals(id))
-			return false;
+
+		for (int i = 0; i < users.size(); i++) {
+			User user = users.get(i);
+			if (user.getId().equals(id))
+				return false;
 		}
 		return true;
 	}
+
 	public int checkUserInfo(String id, String pw) {
 		int index = -1;
-		for(int i = 0 ; i<users.size();i++) {
+		for (int i = 0; i < users.size(); i++) {
 			User user = users.get(i);
-			if(user.getId().equals(id)&&user.getPw().equals(pw))
+			if (user.getId().equals(id) && user.getPw().equals(pw))
 				return index;
 		}
 		return index;
